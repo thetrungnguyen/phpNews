5f091825d8426ddaec9a16a8e8f8b910cf6a5776
Nguyễn Thế Trung <nguyen.thetrung0794@gmail.com>
2019-03-07 09:16:04 +0700

    First Commit
    Đã tạo model cho các bảng.

---
+++ WebTinTuc/app/Slide.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Slide extends Model
+{
+    //Do bảng slide k liên kết với bảng khác nên k cần fuction
+    public $table = "Slide";
+}
---
+++ WebTinTuc/routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+use App\TheLoai;
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('test',function(){
+    $theloai = TheLoai::find(1);
+    foreach($theloai->loaitin as $loaitin){
+        echo $loaitin->Ten."<br>";
+    }
+});
+
